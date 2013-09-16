bd787021f63aa5bcc4728d1368f517d030c2204f
unknown <NoyesMa@SACHDEW7.instron.com>
2013-09-16 16:09:27 +0100

    Add boost::any example

---
+++ boost_any_example/employee.h
@@ -0,0 +1,38 @@
+//          Copyright Malcolm Noyes 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef EMPLOYEE_BOOST_ANY_EXAMPLE_H
+#define EMPLOYEE_BOOST_ANY_EXAMPLE_H
+
+#include <string>
+
+class employee
+{
+public:
+    employee(const std::string& name, double salary)
+        : m_name(name), m_salary(salary)
+    {}
+    std::string get_name() const
+    {
+        return m_name;
+    }
+    double get_pay() const {return m_salary;}
+private:
+    std::string m_name;
+    double m_salary;
+};
+
+class chairman : public employee
+{
+public:
+    chairman(const std::string& name, double salary, double bonus)
+        : employee(name, salary), m_bonus(bonus)
+    {}
+    double get_bonus() const {return m_bonus;}
+private:
+    double m_bonus;
+};
+
+#endif
---
+++ boost_any_example/main.cpp
@@ -0,0 +1,86 @@
+//          Copyright Malcolm Noyes 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include "employee.h"
+#include <boost/any.hpp>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <cassert>
+
+class list_employees
+{
+public:
+    list_employees(std::ostream& os)
+        : m_os(os)
+    {}
+    void print(std::vector<boost::any>& database)
+    {
+        for( std::vector<boost::any>::iterator iter = database.begin(); iter != database.end(); ++iter )
+        {
+            boost::any& a = *iter;
+            employee * emp = boost::any_cast<employee>(&a);
+            if(emp != NULL )
+            {
+                m_os << emp->get_name() << ", " << emp->get_pay() << std::endl;
+            }
+            // Ooops!, we forgot the chairman...
+        }
+    }
+private:
+    std::ostream& m_os;
+};
+
+class payroll
+{
+public:
+    double calculate_total_pay(std::vector<boost::any>& database, int month)
+    {
+        double total = 0.0;
+        for( std::vector<boost::any>::iterator iter = database.begin(); iter != database.end(); ++iter )
+        {
+            boost::any& a = *iter;
+            // works for 'Fred', fails for 'Bert'
+            employee * emp = boost::any_cast<employee>(&a);
+            if(emp != NULL )
+            {
+                total += emp->get_pay();
+            }
+            // works for 'Bert', as expected
+            chairman * chair = boost::any_cast<chairman>(&a);
+            if(chair != NULL && month == 11)
+            {
+                total += chair->get_bonus();
+            }
+        }
+        return total;
+    }
+};
+
+int main()
+{
+    std::vector<boost::any> database;
+    database.push_back(employee("Fred", 100.0));
+    database.push_back(chairman("Bert", 200.0, 1000.0));
+
+    {
+        payroll pr;
+        double result = pr.calculate_total_pay(database, 11);
+        std::cout << "Payroll (should be 1300.0): " << result << std::endl;
+        // fails
+        //assert(result == 1300.0);
+    }
+    {
+        std::ostringstream oss;
+        list_employees li(oss);
+        li.print(database);
+        std::string result = oss.str();
+        assert(result.find("Fred") != std::string::npos);
+        std::cout << "Print (should have 'Fred' & 'Bert'): " << std::endl << result << std::endl;
+        // fails
+        //assert(result.find("Bert") != std::string::npos);
+    }
+    return 0;
+}
